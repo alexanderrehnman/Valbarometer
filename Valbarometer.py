d8d02a911d65b57cc6b2b383d1ffd2f07ca577c2
Alexander R <alexanderrehnman2004@gmail.com>
2022-05-20 12:55:28 +0200

    test

--- Valbarometer.py
+++ Valbarometer.py
@@ -1,3 +1,7 @@
+# imports
+
+# globals
+points = [0,0]
 
 
 class Party:
@@ -12,13 +16,9 @@ class Party:
     
     def get_answers(self):
         return self.answers
-            
-points = [0,0]
-
-
-#Få den här klassen att funka 
+        
 class Question:
-    def __init__(self, question : str, answer : str, choices : list()):
+    def __init__(self, question : str, choices : str, answer : list()):
         self.choices = choices
         self.question = question
         self.answer = answer
@@ -30,21 +30,19 @@ class Question:
         return self.answer
 
     def get_choices(self):
+        
+
         return self.choices
-    
-    
-    #Hur det skrivs ut:
+
+        
+
     def play_question(self, number: int):
-        global points
+        """What the user sees when the code is executed, 
+            and how the points get added to every party. 
 
-        #Skapar de olika partierna
-        global lst
-        p1 = ("mod", ["a", "b", "c", "c", "b"])
-        p2 = ("soc", ["b", "c", "c", "a", "b"])
-        p3 = ("mp" , ["a", "c"])
-        lst = [p1, p2, p3]
-
-       
+        Args:
+            number (int): Saving the points in number(list) if answer = party.get_answers.
+        """
         print("Frågan:")
         print(self.question)
         print()
@@ -53,64 +51,95 @@ class Question:
             print(i)
         print()
         answer = input("Svara här: ")
-        i = 0
-        for partie in lst :
-            if answer == partie.get_answer[number]:
-                points[i] = points[i] + 1
-            i +1
-                
-                       
-#Ta in frågorna och splitta upp dem i fråga, svar, alternativ
-def load_question():
-    questions = []
 
-    #försök att få split att fungera!!!
-    with open("", "r", encoding="utf8") as f:
+        # lägger till poäng i de partiet som matchar med svaret
+        i = 0
+    
+        for party in lst :
+            if answer == party.get_answers()[number]:
+                points[i] = points[i] + 1
+            i = i +1
+                                  
+
+def load_question():
+    """file manager that takes information from a file and then splits it into question objects.
+
+    Returns:
+        questions(list): saves the questions in a list from the file that later gets splited into questions and answers.
+            
+    """
+    questions = []
+    with open("valfragor.txt", "r", encoding="utf8") as f:
         for line in f.readlines():
             selection = line.split("/")
             quiz = Question(selection[0],
-                            selection[1],
-                            selection[2].split(","))
-            
+                            selection[2].split(","),
+                            selection[1])
+
+
             questions.append(quiz)
     return questions
 
 
 
-#mainfunktionen som pratar med användaren.
-def main():
+# mainfunktionen som pratar med användaren.
+def main(): 
+
+    """_summary_
+    """
+
+    global points
+    points = [0, 0, 0]
+
+    # Skapar de olika partierna
+    global lst 
+    p1 = Party("djurpartiet", ["a", "c", "a", "c", "b"])
+    p2 = Party("spelpartiet", ["b", "a", "b", "c", "c"])
+    p3 = Party("vinterpartiet", ["c", "c", "c", "a", "b"])
+    lst = [p1, p2, p3]
+
     round = 1    
+    run = True
+
+    while run:
+        start = input(f"\nTryck Enter för att starta: ")
+        print()
+        print()
+        
+        if "" in start:
+            pass
+
+        question = load_question()
+        for round in range(5):
+            question[round].play_question(round)
+        
+        # går igenom points för att hitta platsen för vinnaren
+        n = 0
+        tie = False
+        global winningParty 
+        for i in range(len(points)-1) :
+            if points[i+1] > points[i]:
+                n = i +1
+                tie == False
+            if points[i+1] == points[i] and points[n] <= points[i]:
+                tie = True
 
 
-    while True:
-            start = input(f" är antalet frågor som du har valt.\nTryck Enter för att starta: ")
-            print()
-            print()
-            
-            if "" in start:
-                pass
+        # tar ut namnet till vinnande parti
+        if tie == True:
+            winningParty = lst[n].get_name() + " & " + lst[n+1].get_name()
+        if tie == False:
+            winningParty= lst[n].get_name()
+        run = False
 
-            question = load_question()
-            for round in range(5):
-                question[round].play_question(round)
-            
-            #går igenom points för att hitta platsen för vinnaren
-            n = 0
-            for i in range(len.points) :
-                if points[i+1] > points[i]:
-                    n = i +1
-            global winningPartie 
-            #tar ut namnet till vinnande parti
-            winningPartie= lst[n].get_name()
-
-
+    
 
 if __name__ == "__main__":
-    print("Hej och välkomen till Alex quiz!")
+    print("Valbarometer!")
     main()
     
 
 
     print("-------------------")
-    print(f"Tack för att du spelade du fick {winningPartie} poäng")
+    print(f"Tack för att du körde vårat program, ditt parti blev: {winningParty}")
 
